9cdf2b3ad5c579e867ff907bc3270ede7010b374
vitebo <vitebo@hotmail.com>
2020-08-26 11:17:18 -0300

    add zilla

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['~/sass/global.scss'],
   /*
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,19 @@
   <ul>
     <li v-for="post of posts" :key="post.slug">
       <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
+      <ZButton>Primary</ZButton>
+      <ZButton variant="secondary">Secondary</ZButton>
     </li>
   </ul>
 </template>
 
 <script>
+import { ZButton } from '@quero/zilla-vue'
+
 export default {
+  components: {
+    ZButton,
+  },
   async asyncData({ $content }) {
     const posts = await $content('blog').fetch()
     return { posts }
