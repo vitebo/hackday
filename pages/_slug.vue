9d3603b519403b85ae374c6ca94f343b638a4dbe
vitebo <vitebo@hotmail.com>
2020-08-26 00:42:13 -0300

    basic example

---
+++ pages/_slug.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{ post.title }}</h2>
+    <NuxtContent :document="post" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    try {
+      const post = await $content('blog', params.slug).fetch()
+      return { post }
+    } catch (e) {
+      error({ message: 'Blog Post not found' })
+      return { post: null }
+    }
+  },
+}
+</script>
