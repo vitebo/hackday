1f203f6634b7bb2c29d43d838823c15bf7d89703
vitebo <vitebo@hotmail.com>
2020-08-26 00:23:31 -0300

    config netlify cms

--- pages/index.vue
+++ pages/index.vue
@@ -26,7 +26,15 @@
 </template>
 
 <script>
-export default {}
+export default {
+  head() {
+    return {
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
+      ],
+    }
+  },
+}
 </script>
 
 <style>
